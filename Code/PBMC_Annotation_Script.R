feca5d0cc56d9f9700426cea53334bf42c748a87
tylerjacksonbio <52325304+tylerjacksonbio@users.noreply.github.com>
2025-02-14 21:12:03 -0600

    Update PBMC_Annotation_Script.R

--- Code/PBMC_Annotation_Script.R
+++ Code/PBMC_Annotation_Script.R
@@ -1,4 +1,4 @@
-##### Annotation using of Whole Data using Azimuth. Gamma-delta T-cell Annotation also Included, Annotated using the Module Score #####
+##### Annotation using of Whole Data using Azimuth. Gamma-delta T-cell Annotation also Included, Annotated using a Module Score of Known Gamma-delta T-cell Marker Genes #####
 # Importing required libraries
 library(Seurat)
 library(sceasy)
@@ -12,6 +12,7 @@ set.seed(42)
 PBMC_filtered_harmony <- readRDS('/Users/tylerjackson/OneDrive - Baylor College of Medicine/Hongjie_Li_Lab_Documents/PBMC_Data_Bin_Su/PBMC_Dataset/Final_Datasets/Final_Annotated_Data/PBMC_filtered_harmony.rds')
 PBMC_filtered_harmony <- readRDS('/Users/tylerjackson/OneDrive - Baylor College of Medicine/Hongjie_Li_Lab_Documents/PBMC_Test_Script/Final_Data_Upload_Analysis/Merged_adata.rds')
 
+############################################# Running Azimuth #############################################
 # The .h5Seurat format is required for using the Azimuth annotation program
 SaveH5Seurat(PBMC_filtered_harmony, 'PBMC_filtered_harmony.h5Seurat')
 
@@ -87,7 +88,7 @@ plot_grid(
 )
 dev.off()
 
-##### Reannotating the data using Azimuth predictions and marker genes #####
+############################################# Reannotating the data using Azimuth predictions and marker genes #############################################
 # Find clusters at the desired resolution and set up final annotation metadata
 PBMC_filtered_harmony <- readRDS('/Users/tylerjackson/OneDrive - Baylor College of Medicine/Hongjie_Li_Lab_Documents/PBMC_Test_Script/Final_Data_Upload_Analysis/Merged_adata.rds')
 PBMC_filtered_harmony<-FindNeighbors(PBMC_filtered_harmony, reduction = 'harmony', dims = 1:50)
@@ -172,7 +173,7 @@ sceasy::convertFormat(
   outFile = file.path(output_path, "Final_annotated_object.h5ad")
 )
 
-##### Save the final annotated object used for downstream analysis #####
+############################################# Save the final annotated object used for downstream analysis #############################################
 saveRDS(PBMC_filtered_harmony, file.path(output_path, "Merged_adata.rds"))
 sceasy::convertFormat(
   PBMC_filtered_harmony,
@@ -181,8 +182,7 @@ sceasy::convertFormat(
   outFile = file.path(output_path, "Merged_adata.h5ad")
 )
 
-##### Whole Data Cell Proportion Analysis and Preliminary Gamma-delta T-cell Analysis #####
-##### Whole data cell proportion analysis #####
+############################################# Whole data cell proportion analysis #############################################
 # Set working directory and define color palette
 setwd('/Users/tylerjackson/OneDrive - Baylor College of Medicine/Hongjie_Li_Lab_Documents/PBMC_Data_Bin_Su/PBMC_Plots/gdT_cell_annotation')
 sample_colors <- c('HC-unstim' = '#9FBDD3', 'HC-stim' = '#5A6EDC', 
@@ -211,7 +211,7 @@ for (sample_name in names(samples)) {
 }
 dev.off()
 
-##### Annotating and reclustering the gdT-cells #####
+############################################# Annotating and reclustering the gdT-cells #############################################
 # Annotating gamma-delta T-cells
 PBMC_filtered_harmony$orig.ident <- factor(PBMC_filtered_harmony$orig.ident, 
                                            levels = names(sample_colors))
@@ -366,3 +366,38 @@ DimPlot(Gdt_cell_clusters, reduction = 'umap', group.by = 'seurat_clusters', lab
   VlnPlot(Gdt_cell_clusters, features = 'gdt_Module_Score1', assay = 'RNA')
 dev.off()
 
+############################################# Session Info #############################################
+# R version 4.4.2 (2024-10-31)
+# Platform: aarch64-apple-darwin20
+# Running under: macOS Sequoia 15.2
+
+# Matrix products: default
+# BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/lib/libRlapack.dylib;  LAPACK version 3.12.0
+
+# locale:
+# [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+
+# time zone: America/Monterrey
+# tzcode source: internal
+
+# attached base packages:
+# [1] stats     graphics  grDevices datasets  utils     methods   base     
+
+# other attached packages:
+# [1] cowplot_1.1.3      patchwork_1.3.0    sceasy_0.0.7       reticulate_1.40.0  Seurat_5.2.1       SeuratObject_5.0.2 sp_2.2-0          
+
+# loaded via a namespace (and not attached):
+#   [1] deldir_2.0-4           pbapply_1.7-2          gridExtra_2.3          rlang_1.1.5            magrittr_2.0.3         RcppAnnoy_0.0.22       matrixStats_1.5.0      ggridges_0.5.6        
+#   [9] compiler_4.4.2         spatstat.geom_3.3-5    png_0.1-8              vctrs_0.6.5            reshape2_1.4.4         stringr_1.5.1          pkgconfig_2.0.3        fastmap_1.2.0         
+#  [17] promises_1.3.2         purrr_1.0.4            jsonlite_1.8.9         goftest_1.2-3          later_1.4.1            spatstat.utils_3.1-2   irlba_2.3.5.1          parallel_4.4.2        
+#  [25] cluster_2.1.8          R6_2.6.0               ica_1.0-3              stringi_1.8.4          RColorBrewer_1.1-3     spatstat.data_3.1-4    parallelly_1.42.0      spatstat.univar_3.1-1 
+#  [33] lmtest_0.9-40          scattermore_1.2        Rcpp_1.0.14            tensor_1.5             future.apply_1.11.3    zoo_1.8-12             sctransform_0.4.1      httpuv_1.6.15         
+#  [41] Matrix_1.7-2           splines_4.4.2          igraph_2.1.4           tidyselect_1.2.1       rstudioapi_0.17.1      abind_1.4-8            spatstat.random_3.3-2  codetools_0.2-20      
+#  [49] miniUI_0.1.1.1         spatstat.explore_3.3-4 listenv_0.9.1          lattice_0.22-6         tibble_3.2.1           plyr_1.8.9             shiny_1.10.0           ROCR_1.0-11           
+#  [57] Rtsne_0.17             future_1.34.0          fastDummies_1.7.5      survival_3.8-3         polyclip_1.10-7        fitdistrplus_1.2-2     pillar_1.10.1          BiocManager_1.30.25   
+#  [65] KernSmooth_2.23-26     renv_1.1.1             plotly_4.10.4          generics_0.1.3         RcppHNSW_0.6.0         ggplot2_3.5.1          munsell_0.5.1          scales_1.3.0          
+#  [73] globals_0.16.3         xtable_1.8-4           glue_1.8.0             lazyeval_0.2.2         tools_4.4.2            data.table_1.16.4      RSpectra_0.16-2        RANN_2.6.2            
+#  [81] dotCall64_1.2          grid_4.4.2             tidyr_1.3.1            colorspace_2.1-1       nlme_3.1-167           cli_3.6.4              spatstat.sparse_3.1-0  spam_2.11-1           
+#  [89] viridisLite_0.4.2      dplyr_1.1.4            uwot_0.2.2             gtable_0.3.6           digest_0.6.37          progressr_0.15.1       ggrepel_0.9.6          htmlwidgets_1.6.4     
+#  [97] farver_2.1.2           htmltools_0.5.8.1      lifecycle_1.0.4        httr_1.4.7             mime_0.12              MASS_7.3-64   
